520a98d0c7c92bd2fc8a356afbcd1b0fe378d656
Kori G <kordillia@gmail.com>
2014-12-21 21:42:35 -0800

    adding blog

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
+    url(r'^markdown', include('django_markdown.urls')),
 )
